a9ed189a764f696485ecd02e4900bb4673465cb4
Pritam991 <pritammanijoy2001@gmail.com>
2023-03-05 19:22:40 +0530

    Middleware

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,10 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { JobsController } from "./controllers/jobs.controller";
 import { JobsService } from "./services/jobs.service";
+import { UserAgentMiddleware, UserAgentOptions } from '../middlewares/user-agent.middleware';
+import { InterviewsController } from './controllers/interviews.controller';
 
 @Module({
-  controllers: [JobsController],
-  providers: [JobsService],
+  controllers: [JobsController, InterviewsController],
+  providers: [JobsService,
+  {
+    provide: UserAgentOptions,
+    useValue: ["chrome", "firefox", "Postman"],
+  },
+],
 })
-export class JobsModule {}
+export class JobsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserAgentMiddleware).forRoutes("jobs");
+  }
+}
